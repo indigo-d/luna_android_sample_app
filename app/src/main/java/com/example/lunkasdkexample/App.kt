397b604b0b29873c6df515e3f72b3bb48635cdb4
Sebastian Rodykow <sebastian.rodykow@brightinventions.pl>
2022-09-23 08:17:43 +0200

    Init commit

---
+++ app/src/main/java/com/example/lunkasdkexample/App.kt
@@ -0,0 +1,15 @@
+package com.example.lunkasdkexample
+
+import android.net.Uri
+import com.luna.sdk.luna.gateway.LunaGateway
+import com.luna.sdk.network.model.router.Router
+import com.luna.sdk.utils.base.LunaGatewayApplication
+
+
+class App: LunaGatewayApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/com/example/lunkasdkexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lunkasdkexample
+
+import android.os.Bundle
+import com.luna.sdk.utils.base.LocationPermissionActivity
+
+class MainActivity : LocationPermissionActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
